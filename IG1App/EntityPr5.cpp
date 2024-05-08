7da25365c534c0cbb1977bc2693cf78b35ed3604
AlvarezIglesias <60479580+AlvarezIglesias@users.noreply.github.com>
2024-05-08 17:24:16 +0200

    pr5 init

---
+++ IG1App/EntityPr5.cpp
@@ -0,0 +1,9 @@
+#include "Entity.h"
+#include "EntityPr4.h"
+#include "EntityPr1.h"
+#include "Texture.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace glm;
---
+++ IG1App/EntityPr5.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <vector>
+#include "Entity.h"
+#include "Mesh.h"
